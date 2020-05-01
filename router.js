60b9c0794e8893ede79bfb49066fbb05db882f4c
MarkusJorroch <markusactionjackson@gmail.com>
2020-05-01 09:34:16 +0200

    first commit

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 	// res.send({ response: 'Server is up and running.' }).status(200);
-	res.send('<h3>Welcome to Jorroch-Consulting experimental Node.js Backend-Server. It´s up and running.</h3>').status(200);
+	res.send('<h3>Welcome to Jorroch-Consulting experimental Node.js Crypto Backend-Server. It´s up and running.</h3>').status(200);
 });
 
 module.exports = router;
